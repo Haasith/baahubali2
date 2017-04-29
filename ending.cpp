bf93693d722e59b99e500e1c1bc4208ccca1e62c
teja <saiteja.undi1996@gmail.com>
2017-04-29 15:11:56 +0530

    windows changed

--- ending.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-main()
-{
-int a=20;
-int &b=a;
-cout<<"the reference variable is:"<<b;
-}
--- programc.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-float a,b,c;
-scanf("%f%f",&a,&b);
-c=a/b;
-printf("c=%f",c);
-}
